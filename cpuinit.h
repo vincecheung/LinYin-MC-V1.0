fcd89be66fb5ca40262e9709b0c65d719ca838d6
VinceCheung <vincecheung1117@gmail.com>
2017-06-14 09:17:47 +0800

    Create cpuinit.h

---
+++ cpuinit.h
@@ -0,0 +1,12 @@
+
+#ifndef CPUINIT_H
+#define CPUINIT_H
+//*********************************************************
+//                      IO_init                           *
+//      初始化包含文件定义，主要进行CPU的功能初始化       *
+//*********************************************************
+extern void IO_init(void);
+extern void T1_Init(void);
+extern void ADC_init(void);
+
+#endif
